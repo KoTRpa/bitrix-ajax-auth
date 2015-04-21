7b134983080298a35e54dd157bf6e1fd9a9a2c53
Max Kashkin <4KoTRpa@gmail.com>
2015-04-21 19:02:00 +0800

    Added some html examples. And service.js is extended. Now it supports ajax status with message

--- js/service.js
+++ js/service.js
@@ -21,47 +21,121 @@ var Service = {};
 	var
 		// private methods
 		sendAjax = function(url, data, method, callback) {// @TODO: errors handling
-			showLoader();
-			switch (method){
-				case 'post':
-				case 'put':
-				case 'delete': 
-					data.method = method;
-					$.ajax({
-						url: url,
-						type: 'post',
-						data: data,
-						success: function (res) {
-							hideLoader();
-							callback(res);
-						}
-					});
-					break;
+			var 
+				url = '', 
+				data = {}, 
+				method = 'get', 
+				msg = '', 
+				callback = function(){}, 
+				request_type = 'get';
 
-				default:
-					$.getJSON(url, data, function(res){
-						hideLoader();
-						callback(res);
-					});
+			switch (arguments.length)
+			{
+				case 1: // just url
+					url = arguments[0];
+					break;
+				case 2: // url and data
+					url = arguments[0];
+					data = arguments[1];
+					break;
+				case 3: // url, data, and callback
+					url = arguments[0];
+					data = arguments[1];
+					callback = arguments[2];
+					break;
+				case 4: // url, data, method, and callback
+					url = arguments[0];
+					data = arguments[1];
+					method = arguments[2];
+					callback = arguments[3];
+					break;
+				case 5: // url, data, method, msg and callback
+					url = arguments[0];
+					data = arguments[1];
+					method = arguments[2];
+					msg = arguments[3];
+					callback = arguments[4];
+					break;
 			}
+
+			showLoader(msg);
+			
+			if (method != 'get')
+				data.method = request_type = method;
+
+			$.ajax({
+				url: url,
+				type: request_type,
+				data: data,
+				success: function (res) {
+					hideLoader();
+					callback(res);
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					hideLoader();
+					callback({ajax_error: textStatus}, errorThrown);
+				}
+			});
 		},
 
-		showLoader = function(){
+		uploadFilesAjax = function(url, data, callback) {
+			showLoader('Загрузка файлов');
+			// @TODO: upload progress bar
+			$.ajax({
+				url: url,
+				type: 'POST',
+				data: data,
+				cache: false,
+				dataType: 'json',
+				processData: false, // Don't process the files
+				contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+				success: function(response)
+				{
+					if(response.success)
+					{
+						callback();
+					}
+					else
+					{
+						// Handle errors here
+						console.log('ERRORS: ' + response.error);
+						callback(response);
+						hideLoader();
+					}
+				},
+				error: function(jqXHR, textStatus, errorThrown)
+				{
+					// Handle errors here
+					console.log('ERRORS: ' + textStatus);
+					callback(textStatus);
+					hideLoader();
+				}
+			});
+		},
+
+		showLoader = function(msg){
+			if(msg != undefined)
+				$('.ajax-loader .ajax-info').html(msg);
+			else
+				$('.ajax-loader .ajax-info').html('');
+
 			$('.ajax-loader').addClass('active');
 		},
+
 		hideLoader = function(){
+			$('.ajax-loader .ajax-info').html('');
 			$('.ajax-loader').removeClass('active');
 		},
 
-		showAlert = function(msg, status, cont) {
+		showAlert = function(msg, status, $cont) {
 			if (status == undefined)
 				status = 'error';
 
 			if (msg instanceof Array)
 				msg = new EJS({url: '/include/ejs/errors_list.ejs'}).render({errors:msg});
 
-			if (cont != undefined)
-				cont.find('.alert').addClass('alert-' + status).html(msg);
+			if ($cont != undefined)
+				$cont.find('.alert').addClass('alert-' + status).html(msg);
 			else
 				$('.alert').addClass('alert-' + status).html(msg);
 		};
@@ -71,8 +145,16 @@ var Service = {};
 		sendAjax(url, data, 'get', callback);
 	}
 
+	context.getAjaxMsg = function(url, data, msg, callback){
+		sendAjax(url, data, 'get', msg, callback);
+	}
+
 	context.postAjax = function(url, data, callback){
 		sendAjax(url, data, 'post', callback);
+	}
+
+	context.uploadFiles = function(url, data, callback){
+		uploadFilesAjax(url, data, callback);
 	}
 
 	context.validate = function(val) {
@@ -84,19 +166,26 @@ var Service = {};
 		return re.test(email);
 	}
 
-	context.showError = function(msg, cont) {
-		showAlert(msg, 'error', cont);
+	context.setLoaderText = function(txt){
+		if(typeof txt === 'undefined')
+			txt = '';
+
+		$('.ajax-loader .ajax-info').html(txt);
 	}
 
-	context.showSuccess = function(msg, cont) {
-		showAlert(msg, 'success', cont);
+	context.showError = function(msg, $cont) {
+		showAlert(msg, 'error', $cont);
 	}
 
-	context.hideAlert = function(cont) {
+	context.showSuccess = function(msg, $cont) {
+		showAlert(msg, 'success', $cont);
+	}
+
+	context.hideAlert = function($cont) {
 		var $alert;
 
-		if (cont != undefined)
-			$alert = cont.find('.alert');
+		if ($cont != undefined)
+			$alert = $cont.find('.alert');
 		else
 			$alert = $('.alert');
 
